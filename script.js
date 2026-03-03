20916c6067d8e74517637455405a51413cbfe8d3
Sejalkeserwani <sejalkeserwani0403@gmail.com>
2026-03-03 15:00:13 +0530

    Initial commit - medGO project setup

---
+++ script.js
@@ -0,0 +1,28 @@
+function getLocation() {
+    if (navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    }else{
+        document.getElementById("location").innerHTML="Geolocation is not supported by this browser.";
+    }
+}
+ function showPosition(position) {
+    document.getElementById("location").innerHTML="Latitude:"+ position.coords.latitude + position.coords.longitude;  
+ }
+ function showError(error) {
+    switch(error.code){
+        case error.PERMISSION_DENIED:
+ document.getElementById("location").innerHTML="User denied the request for Geolocation."
+ break;
+      case error.PERMISSION_UNAVAILABLE:
+document.getElementById("location").innerHTML="Location information is unavailable."
+        break;
+        case error.TIMEOUT:
+document.getElementById("Location").innerHTML="The request to get user location timed out."
+        break;
+        case error.UNKNOWN_ERROR:
+document.getElementById("location").innerHTML="An unknown error occurred."
+break;
+
+    }
+    
+ }
